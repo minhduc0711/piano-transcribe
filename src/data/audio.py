33e9a8c8b5bdd07abaed993e72c147810bc81ce4
Duc Pham <phamminhduc0711@gmail.com>
2020-11-23 14:39:15 +0700

    Add example of audio transform using Madmom (#4)

--- src/data/audio.py
+++ src/data/audio.py
@@ -1,6 +1,9 @@
+import numpy as np
+
 import torch
 import torch.nn as nn
 import torchaudio
+import madmom
 
 from src.layer_utils import Lambda
 
@@ -29,3 +32,35 @@ onf_transform = nn.Sequential(
     Lambda(lambda x: torch.log(torch.clamp(x, min=1e-5))),
     Lambda(lambda x: torch.transpose(x, 0, 1))
 )
+
+
+class MadmomSpectrogram:
+    """
+    Extract audio spectrogram using madmom
+    """
+    def __init__(self, hop_length, sample_rate=16000):
+        self.hop_length = hop_length
+        self.sample_rate = sample_rate
+
+    def __call__(self, x):
+        x = madmom.audio.signal.Signal(x.numpy(), sample_rate=self.sample_rate)
+        audio_options = dict(
+            num_channels=1,
+            sample_rate=self.sample_rate,
+            filterbank=madmom.audio.filters.LogarithmicFilterbank,
+            frame_size=4096,
+            fft_size=4096,
+            hop_size=self.hop_length,  # 25 fps
+            num_bands=48,
+            fmin=30,
+            fmax=8000.0,
+            fref=440.0,
+            norm_filters=True,
+            unique_filters=True,
+            circular_shift=False,
+            norm=True
+        )
+
+        # dt = float(audio_options['hop_size']) / float(audio_options['sample_rate'])
+        sig = madmom.audio.spectrogram.LogarithmicFilteredSpectrogram(x, **audio_options)
+        return torch.from_numpy(np.array(sig.data))
